4a64254022c36ac86f67cc75565222b43563cbf7
Micah Bales <micahbales@gmail.com>
2017-07-04 13:26:42 -0400

    add public folder to serve up static files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 // view engine setup
 app.set('view engine', 'pug');
 
+// serve up static files from 'public'
+app.use(express.static('public'));
+
 // takes raw requests and converts them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
