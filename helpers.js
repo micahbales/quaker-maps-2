c5485dfd9eb24bce4c15a8f59c03a8bce3ee6751
Micah Bales <micahbales@gmail.com>
2017-07-10 14:49:39 -0400

    use fontawesome (via cdn) to display navbar icons

--- helpers.js
+++ helpers.js
@@ -7,8 +7,8 @@ exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // auto-populate menu items
 exports.menu = [
-  { slug: '/map', title: 'Maps', icon: 'map', },
-  { slug: '/meetings', title: 'Meetings', icon: 'meeting' }
+  { slug: '/map', title: 'Maps', fa: 'globe', },
+  { slug: '/meetings', title: 'Meetings', fa: 'users' }
 ];
 
 // dump raw JSON into the view, for debugging purposes
