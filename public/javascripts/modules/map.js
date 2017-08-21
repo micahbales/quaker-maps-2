c558eda185eaa25e9d5839a0e28178f5b07c4a3c
Micah Bales <micahbales@gmail.com>
2017-08-21 15:40:42 -0400

    Abstract mapOptions

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
+import { $ } from './bling';
+
+const mapOptions = {
+  zoom: 4,
+  center: {lat: 39.8283, lng: -98.5795},
+  maxZoom: 16
+}
+
 function initMap(mapDiv) {
-  var map = new google.maps.Map(mapDiv, {
-    zoom: 4,
-    center: {lat: 39.8283, lng: -98.5795}
-  });
+  var map = new google.maps.Map(mapDiv, mapOptions);
   var marker = new google.maps.Marker({
     position: {lat: 39.8283, lng: -98.5795},
     map: map
